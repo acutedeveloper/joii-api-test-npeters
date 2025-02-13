cea6692fba09766f80add9fbef76936c5678e5dc
acute.developer <acute.developer@nigelmpeters.co.uk>
2025-02-13 17:23:48 +0000

    Feat: Adding user login method

--- src/app/Http/Controllers/UserLoginController.php
+++ src/app/Http/Controllers/UserLoginController.php
@@ -2,11 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserLoginController extends Controller
 {
     public function register(Request $request) {
+        $userInfo = $request->validate([
+            'name'=>'required|string',
+            'email'=>'required|string|email|unique:users',
+            'password'=>'required|min:8'
+        ]);
+
+        User::create([
+            'name' => $userInfo['name'],
+            'email' => $userInfo['email'],
+            'password' => Hash::make($userInfo['password']),
+        ]);
         return response()->json([
             'message' => 'User Created ',
         ]);
