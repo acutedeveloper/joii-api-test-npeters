856a71c8c76bfef37997a54bb4f7449ea4cd6c8d
acute.developer <acute.developer@nigelmpeters.co.uk>
2025-02-13 17:27:59 +0000

    Feat: Adding the login method

--- src/app/Http/Controllers/UserLoginController.php
+++ src/app/Http/Controllers/UserLoginController.php
@@ -26,8 +26,22 @@ class UserLoginController extends Controller
     }
 
     public function login(Request $request) {
+        $userLoginInfo = $request->validate([
+            'email'=>'required|string|email',
+            'password'=>'required|min:8'
+        ]);
+
+        $user = User::where('email',$userLoginInfo['email'])->first();
+
+        if(!$user || !Hash::check($userLoginInfo['password'],$user->password)){
+            return response()->json([
+                'message' => 'Invalid Credentials'
+            ],401);
+        }
+
+        $token = $user->createToken($user->name.'-AuthToken')->plainTextToken;
         return response()->json([
-            'message' => 'User logged in',
+            'access_token' => $token,
         ]);
     }
 
