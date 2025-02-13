8d76a1a163a809bc7929ab87178c163761519268
acute.developer <acute.developer@nigelmpeters.co.uk>
2025-02-13 20:13:11 +0000

    Chore: Updating Postman Collection File Chore: Removing uneeded route

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,10 +4,3 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::get('login', function () {
-    return response()->json([
-        'message' => 'User sent back to login page',
-    ]);
-});
